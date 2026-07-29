9ec1f0e680584bc68379c356af2de4b06b489ee6
InnovaeDigital <innovaedigital.media@gmail.com>
2026-07-29 10:05:24 -0300

    Delete backend directory

--- backend/src/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  port: Number(process.env.PORT || 3001),
-  corsOrigin:
-    process.env.CORS_ORIGIN ||
-    'http://localhost:5173,http://127.0.0.1:5173,https://comsoc.pages.dev,https://*.comsoc.pages.dev',
-};
